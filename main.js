6acf20f69ce6c301a8a5ad411adddc6ca059cc1e
tomboerr <tjboerresen@fastmail.com>
2026-09-18 22:43:29 +0200

    added keyboard layout

--- main.js
+++ main.js
@@ -10,6 +10,11 @@ var word = "drama";
 var selectWidth = "5px"
 var normalWidth = "2px"
 
+var keyLayout = [
+    ["Q","W","E","R","T","Z","U","I","O","P"],
+    ["A","S","D","F","G","H","J","K","L"],
+    ["Enter","Y","X","C","V","B","N","M","Backspace"]
+]
 
 window.onload = function() {
     initialize();
@@ -28,49 +33,73 @@ function initialize() {
     word = word.toUpperCase();
     getTile().style.borderWidth = selectWidth;
 
-    document.addEventListener("keyup", (e) => {
-        if (gameOver) return;
-
-        if (/^[a-zA-Z]$/.test(e.key)) {
-            if (col < width){
-                let currTile = getTile()
-                currTile.style.borderWidth = normalWidth;
-                if (currTile.innerText == "") {
-                    currTile.innerText = e.key.toUpperCase();
-                    col += 1;
-                }
-                let nextTile = getTile()
-                nextTile.style.borderWidth = selectWidth;
+    for (let r = 0; r < keyLayout.length; r++){
+        let keyRow = document.createElement("div");
+        keyRow.id = "row" + r.toString();
+        keyRow.classList.add("row");
+        document.getElementById("keyboard").appendChild(keyRow);
+        for (let k = 0; k < keyLayout[r].length; k++){
+            let key = document.createElement("span");
+            key.id = keyLayout[r][k];
+            key.classList.add("key");
+            if (keyLayout[r][k] == "Enter" || keyLayout[r][k] == "Backspace") {
+                key.classList.add("wide");
             }
+            if (keyLayout[r][k] == "Backspace"){
+                key.innerText = "⌫";
+            }
+            else{
+                key.innerText = keyLayout[r][k];
+            }
+            document.getElementById("row" + r.toString()).appendChild(key);
         }
-        else if (e.key == "Backspace") {
-            if (col < width){
-                let currTile = getTile()
-                currTile.style.borderWidth = normalWidth;
-            }
-            if (0 < col && col <= width){
-                col -= 1;
+    }
+
+    document.addEventListener("keyup", (e) => pressKey(e.key))
+}
+
+function pressKey(key){
+    if (gameOver) return;
+
+    if (/^[a-zA-Z]$/.test(key)) {
+        if (col < width){
+            let currTile = getTile()
+            currTile.style.borderWidth = normalWidth;
+            if (currTile.innerText == "") {
+                currTile.innerText = key.toUpperCase();
+                col += 1;
             }
             let nextTile = getTile()
-            nextTile.innerText = "";
             nextTile.style.borderWidth = selectWidth;
         }
-        else if (e.key == "Enter") {
-            if (col == width){
-                showResult()
-                row += 1;
-                col = 0;
-                if (!gameOver && row < height) {
-                    getTile().style.borderWidth = selectWidth;
-                }
+    }
+    else if (key == "Backspace") {
+        if (col < width){
+            let currTile = getTile()
+            currTile.style.borderWidth = normalWidth;
+        }
+        if (0 < col && col <= width){
+            col -= 1;
+        }
+        let nextTile = getTile()
+        nextTile.innerText = "";
+        nextTile.style.borderWidth = selectWidth;
+    }
+    else if (key == "Enter") {
+        if (col == width){
+            showResult()
+            row += 1;
+            col = 0;
+            if (!gameOver && row < height) {
+                getTile().style.borderWidth = selectWidth;
             }
         }
+    }
 
-        if (!gameOver && row == height){
-            gameOver = true;
-            document.getElementById("answer").innerText = word;
-        }
-    })
+    if (!gameOver && row == height){
+        gameOver = true;
+        document.getElementById("answer").innerText = word;
+    }
 }
 
 function showResult(){
